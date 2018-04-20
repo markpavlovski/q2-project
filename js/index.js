28d1458b4b199adf3299a42a8d43b4f507949c11
Mark Pavlovski <markpavlovski@gmail.com>
2018-04-20 15:18:20 -0700

    moved stuff to index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 // MAIN
 
 // standard global variables
@@ -7,76 +6,112 @@ var keyboard = new THREEx.KeyboardState();
 var clock = new THREE.Clock();
 // custom global variables
 
+var group = new THREE.Group();
 var MovingCube;
 var collidableMeshList = [];
 
 var arrowList = [];
-var directionList = [];
+var unitDirections = {
+	left: new THREE.Vector3(-1, 0, 0),
+	right: new THREE.Vector3(1, 0, 0),
+	up: new THREE.Vector3(0, 0, 1),
+	down: new THREE.Vector3(0, 0, -1)
+}
+
+const availableDirections = {
+	up: true,
+	down: true,
+	left: true,
+	right: true
+}
+
+let lastDirection = null
 
 init();
 animate();
 
 // FUNCTIONS
-function init()
-{
+function init() {
 	// SCENE
 	scene = new THREE.Scene();
 	// CAMERA
-	var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;
-	var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 20000;
-	camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
+	var SCREEN_WIDTH = window.innerWidth,
+		SCREEN_HEIGHT = window.innerHeight;
+	var VIEW_ANGLE = 45,
+		ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT,
+		NEAR = 0.1,
+		FAR = 20000;
+	camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
 	scene.add(camera);
-	camera.position.set(0,150,400);
+	camera.position.set(0, 150, 400);
 	camera.lookAt(scene.position);
 	// RENDERER
-	if ( Detector.webgl )
-		renderer = new THREE.WebGLRenderer( {antialias:true} );
+	if (Detector.webgl)
+		renderer = new THREE.WebGLRenderer({
+			antialias: true
+		});
 	else
 		renderer = new THREE.CanvasRenderer();
 	renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-	container = document.getElementById( 'ThreeJS' );
-	container.appendChild( renderer.domElement );
+	container = document.getElementById('ThreeJS');
+	container.appendChild(renderer.domElement);
 	// EVENTS
 	THREEx.WindowResize(renderer, camera);
-	THREEx.FullScreen.bindKey({ charCode : 'm'.charCodeAt(0) });
+	THREEx.FullScreen.bindKey({
+		charCode: 'm'.charCodeAt(0)
+	});
 	// CONTROLS
-	controls = new THREE.OrbitControls( camera, renderer.domElement );
+	controls = new THREE.OrbitControls(camera, renderer.domElement);
 	// STATS
 	stats = new Stats();
 	stats.domElement.style.position = 'absolute';
 	stats.domElement.style.bottom = '0px';
 	stats.domElement.style.zIndex = 100;
-	container.appendChild( stats.domElement );
+	container.appendChild(stats.domElement);
 	// LIGHT
 	var light = new THREE.PointLight(0xffffff);
-	light.position.set(0,250,0);
+	light.position.set(0, 250, 0);
 	scene.add(light);
 	// FLOOR
-	var floorMaterial = new THREE.MeshBasicMaterial( {color:0x444444, side:THREE.DoubleSide} );
+	var floorMaterial = new THREE.MeshBasicMaterial({
+		color: 0x444444,
+		side: THREE.DoubleSide
+	});
 	var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);
 	var floor = new THREE.Mesh(floorGeometry, floorMaterial);
 	floor.position.y = -0.5;
 	floor.rotation.x = Math.PI / 2;
 	scene.add(floor);
 	// SKYBOX/FOG
-	var skyBoxGeometry = new THREE.CubeGeometry( 10000, 10000, 10000 );
-	var skyBoxMaterial = new THREE.MeshBasicMaterial( { color: 0x9999ff, side: THREE.BackSide } );
-	var skyBox = new THREE.Mesh( skyBoxGeometry, skyBoxMaterial );
+	var skyBoxGeometry = new THREE.CubeGeometry(10000, 10000, 10000);
+	var skyBoxMaterial = new THREE.MeshBasicMaterial({
+		color: 0x9999ff,
+		side: THREE.BackSide
+	});
+	var skyBox = new THREE.Mesh(skyBoxGeometry, skyBoxMaterial);
 	scene.add(skyBox);
 
 	////////////
 	// CUSTOM //
 	////////////
 
-	var cubeGeometry = new THREE.CubeGeometry(50,50,50,1,1,1);
-	var wireMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe:true } );
-	MovingCube = new THREE.Mesh( cubeGeometry, wireMaterial );
+	var cubeGeometry = new THREE.CubeGeometry(50, 50, 50, 1, 1, 1);
+	var wireMaterial = new THREE.MeshBasicMaterial({
+		color: 0xff0000,
+		wireframe: true
+	});
+	MovingCube = new THREE.Mesh(cubeGeometry, wireMaterial);
 	MovingCube.position.set(0, 25.1, 0);
-	scene.add( MovingCube );
+	scene.add(MovingCube);
 
-	var wallGeometry = new THREE.CubeGeometry( 100, 100, 20, 1, 1, 1 );
-	var wallMaterial = new THREE.MeshBasicMaterial( {color: 0x8888ff} );
-	var wireMaterial = new THREE.MeshBasicMaterial( { color: 0x000000, wireframe:true } );
+	var wallGeometry = new THREE.CubeGeometry(100, 100, 20, 1, 1, 1);
+	var wallMaterial = new THREE.MeshBasicMaterial({
+		color: 0x8888ff
+	});
+	var wireMaterial = new THREE.MeshBasicMaterial({
+		color: 0x000000,
+		wireframe: true
+	});
 
 	var wall = new THREE.Mesh(wallGeometry, wallMaterial);
 	wall.position.set(100, 50, -100);
@@ -99,38 +134,52 @@ function init()
 
 }
 
-function clearText()
-{   document.getElementById('message').innerHTML = '..........';   }
+function clearText() {
+	document.getElementById('message').innerHTML = '..........';
+}
 
-function appendText(txt)
-{   document.getElementById('message').innerHTML += txt;   }
+function appendText(txt) {
+	document.getElementById('message').innerHTML += txt;
+}
 
-function animate()
-{
-  requestAnimationFrame( animate );
+function animate() {
+	requestAnimationFrame(animate);
 	render();
 	update();
 }
 
-function update()
-{
+function update() {
 	var delta = clock.getDelta(); // seconds.
 	var moveDistance = 200 * delta; // 200 pixels per second
-	var rotateAngle = Math.PI / 2 * delta;   // pi/2 radians (90 degrees) per second
 
-	if ( keyboard.pressed("A") )
-		MovingCube.rotation.y += rotateAngle;
-	if ( keyboard.pressed("D") )
-		MovingCube.rotation.y -= rotateAngle;
 
-	if ( keyboard.pressed("left") )
-		MovingCube.position.x -= moveDistance;
-	if ( keyboard.pressed("right") )
-		MovingCube.position.x += moveDistance;
-	if ( keyboard.pressed("up") )
-		MovingCube.position.z -= moveDistance;
-	if ( keyboard.pressed("down") )
-		MovingCube.position.z += moveDistance;
+
+
+
+	let possiblePosition = {
+		left: MovingCube.position.clone() + unitDirections.left * moveDistance,
+		right: MovingCube.position.clone() + unitDirections.right * moveDistance,
+		up: MovingCube.position.clone() + unitDirections.up * moveDistance,
+		down: MovingCube.position.clone() + unitDirections.down * moveDistance
+	}
+
+	if (keyboard.pressed("left")) {
+		MovingCube.position.x -= moveDistance * availableDirections.left;
+		lastDirection = 'left'
+	}
+	if (keyboard.pressed("right")) {
+		MovingCube.position.x += moveDistance * availableDirections.right;
+		lastDirection = 'right'
+	}
+	if (keyboard.pressed("up")) {
+		MovingCube.position.z -= moveDistance * availableDirections.up;
+		lastDirection = 'up'
+
+	}
+	if (keyboard.pressed("down")) {
+		MovingCube.position.z += moveDistance * availableDirections.down;
+		lastDirection = 'down'
+	}
 
 	// collision detection:
 	//   determines if any of the rays from the cube's origin to each vertex
@@ -142,23 +191,25 @@ function update()
 
 	clearText();
 
-	for (var vertexIndex = 0; vertexIndex < MovingCube.geometry.vertices.length; vertexIndex++)
-	{
+	for (var vertexIndex = 0; vertexIndex < MovingCube.geometry.vertices.length; vertexIndex++) {
 		var localVertex = MovingCube.geometry.vertices[vertexIndex].clone();
-		var globalVertex = localVertex.applyMatrix4( MovingCube.matrix );
-		var directionVector = globalVertex.sub( MovingCube.position );
+		var globalVertex = localVertex.applyMatrix4(MovingCube.matrix);
+		var directionVector = globalVertex.sub(MovingCube.position);
 
-		var ray = new THREE.Raycaster( originPoint, directionVector.clone().normalize() );
-		var collisionResults = ray.intersectObjects( collidableMeshList );
-		if ( collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() )
+		var ray = new THREE.Raycaster(originPoint, directionVector.clone().normalize());
+		var collisionResults = ray.intersectObjects(collidableMeshList);
+		if (collisionResults.length > 0 && collisionResults[0].distance < directionVector.length()){
+			console.log(lastDirection);
+			console.log(availableDirections);
+			availableDirections[lastDirection]=false;
 			appendText(" Hit ");
+		}
 	}
 
 	controls.update();
 	stats.update();
 }
 
-function render()
-{
-	renderer.render( scene, camera );
+function render() {
+	renderer.render(scene, camera);
 }
